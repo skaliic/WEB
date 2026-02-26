3c4f22a62441e43e49793380d58b9c270e4ead40
Jakub Skála <skalaj24@spstrutnov.cz>
2026-02-26 09:08:54 +0100

    Přidání stránky do navigace.

--- Source/WebApplication1/Controllers/HomeController.cs
+++ Source/WebApplication1/Controllers/HomeController.cs
@@ -36,6 +36,11 @@ namespace WebApplication1.Controllers
             return View();
         }
 
+        public IActionResult NovaStranka()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
