bde992f04561f1ac34171983799dae1662c9ebc9
Jakub Skála <skalaj24@spstrutnov.cz>
2026-03-04 10:28:27 +0100

    pokračování

---
+++ Source/WebApplication1/Models/AccountViewModels.cs
@@ -0,0 +1,12 @@
+﻿ using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Models
+{
+    public class AccountViewModels : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Source/WebApplication1/Program.cs
+++ Source/WebApplication1/Program.cs
@@ -1,29 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSession(); // 👈 přidat
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseRouting();
-
+app.UseSession(); // 👈 přidat (musí být PŘED UseAuthorization)
 app.UseAuthorization();
-
 app.MapStaticAssets();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
