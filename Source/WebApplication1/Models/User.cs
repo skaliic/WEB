c645214571e7c570619a1c544773b352606acce3
Jakub Skála <skalaj24@spstrutnov.cz>
2026-03-26 08:46:49 +0100

    Správný hash hesel

--- Source/WebApplication1/Models/User.cs
+++ Source/WebApplication1/Models/User.cs
@@ -1,18 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations;
-
 namespace WebApplication1.Models
 {
     public class User
     {
         public int Id { get; set; }
-
         [Required]
         public string Name { get; set; } = "";
-
         [Required]
         public string Email { get; set; } = "";
-
         [Required]
         public string Password { get; set; } = "";
+
+        public string PasswordHash { get; set; } = ""; // přidej tento řádek
     }
 }
